8520aaac0daee6f3f518f9424ad636dae8aa330e
Your Name <you@example.com>
2022-06-27 20:24:03 -0300

    subir

--- Servidor-Express.js
+++ Servidor-Express.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
+const fs = require("fs")
 const port = 8080;
+const Contenedor = require("../Mariano-ferrisi-Entrega2.1.js")
 
+/* Contenedor*/
+const archivo = new Contenedor ("../productos.txt");
+const getProductos= archivo.getAll();
 
-
+/*Routes*/
 app.get('/productos', (req, res) => {
-    res.send("Hace frio!");
+    const producto = await getProductos
+    res.send(producto);
 })
 app.get('/productosRandom', (req, res) => {
     res.send("Hace frio!");
+})
+
+app.get("/productoRandom", async (req, res) => {
+    const resultado = await getProductos;
+    const aleatorio= resultado[Math.floor(Math.random() * resultado.length)];
+    res.send(aleatorio) 
 })
 
 app.listen(port, () => {
@@ -16,3 +28,7 @@ app.listen(port, () => {
 })
 
 app.on("error", error => console.log("El error es: " + error));
+
+
+
+
